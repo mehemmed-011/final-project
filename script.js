b7cba576e2557ef4c3716c546934f31f6b4776f4
mehemmed-011 <mehemmedibrahimli0090@gmail.com>
2026-05-24 23:37:05 +0400

    bars

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 let api = "https://dummyjson.com/products/category/";
 let categories = ["smartphones", "mobile-accessories", "laptops"];
 let productBox = document.querySelector(".product-boxes");
+
+
+let bars = document.querySelector(".bars");
+let menuPhone = document.querySelector(".menu-phone");
+let main = document.querySelector("main");
+let state = false;
+bars.addEventListener("click", ()=>{
+    if(!(state)){
+        menuPhone.style.display = "flex"
+        main.style.marginTop = "45vw";
+        state = true;
+    }
+
+    else{
+        menuPhone.style.display = "none"
+        main.style.marginTop = "12vw";
+        state = false;
+    }
+})
