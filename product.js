fcfd6348490efd2a3573e4e51f91ec650efa41ff
mehemmed-011 <mehemmedibrahimli0090@gmail.com>
2026-05-29 11:55:46 +0400

    product-filter

--- product.js
+++ product.js
@@ -2,7 +2,11 @@ let api = "https://dummyjson.com/products/category/";
 let categories = ["smartphones", "mobile-accessories", "laptops"];
 let productBox1 = document.querySelector(".product-boxes");
 let productBox2 = document.querySelector(".product-boxes2");
+let filterBtn = document.querySelectorAll(".chip");
+let select = document.querySelector(".select");
 
+
+//Əsas səhifə
 async function getProducts(){
     let allProducts = [];
     for(let i = 0; i < categories.length; i++){
@@ -39,9 +43,10 @@ async function getProducts(){
         `;
     });
 }
-
 getProducts();
 
+
+//Product səhifə
 async function getProducts2(){
     let allProducts2 = [];
     for(let i = 0; i < categories.length; i++){
@@ -77,5 +82,134 @@ async function getProducts2(){
         `;
     });
 }
-
 getProducts2();
+
+
+//Smartfon hissəsi
+async function smartProduct(){
+    let sProducts = [];
+    let response = await
+    fetch(`${api}smartphones`)
+    .then(res => res.json())
+    .then(data =>{
+        sProducts.push(...data.products);
+    })
+
+    sProducts.forEach(product => {
+
+        productBox2.innerHTML += `
+        
+            <article class="product-card">
+            <div class="product-img">
+                <img src="${product.thumbnail}" alt="#"/>
+            </div>
+            <div class="product-body">
+                <div class="product-title">${product.title}</div>
+                <div class="product-meta">
+                <span>⭐ ${product.rating}</span>
+                <span>${product.tags[0]}</span>
+                </div>
+                <div class="product-price">$${product.price}</div>
+                <button class="add-btn">Səbətə əlavə et</button>
+            </div>
+            </article>
+        
+        `;
+    });
+}
+
+
+//Laptop hissəsi
+async function laptopProduct(){
+    let lProducts = [];
+    let response = await
+    fetch(`${api}laptops`)
+    .then(res => res.json())
+    .then(data =>{
+        lProducts.push(...data.products);
+    })
+
+    lProducts.forEach(product => {
+
+        productBox2.innerHTML += `
+        
+            <article class="product-card">
+            <div class="product-img">
+                <img src="${product.thumbnail}" alt="#"/>
+            </div>
+            <div class="product-body">
+                <div class="product-title">${product.title}</div>
+                <div class="product-meta">
+                <span>⭐ ${product.rating}</span>
+                <span>${product.tags[0]}</span>
+                </div>
+                <div class="product-price">$${product.price}</div>
+                <button class="add-btn">Səbətə əlavə et</button>
+            </div>
+            </article>
+        
+        `;
+    });
+}
+
+
+//Aksesuar hissəsi
+async function accessoriesProduct(){
+    let accProducts = [];
+    let response = await
+    fetch(`${api}mobile-accessories`)
+    .then(res => res.json())
+    .then(data =>{
+        accProducts.push(...data.products);
+    })
+
+    accProducts.forEach(product => {
+
+        productBox2.innerHTML += `
+        
+            <article class="product-card">
+            <div class="product-img">
+                <img src="${product.thumbnail}" alt="#"/>
+            </div>
+            <div class="product-body">
+                <div class="product-title">${product.title}</div>
+                <div class="product-meta">
+                <span>⭐ ${product.rating}</span>
+                <span>${product.tags[0]}</span>
+                </div>
+                <div class="product-price">$${product.price}</div>
+                <button class="add-btn">Səbətə əlavə et</button>
+            </div>
+            </article>
+        
+        `;
+    });
+}
+
+//Filter hissəsi
+filterBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
+        filterBtn.forEach(b => b.classList.remove("active-chip"));
+        btn.classList.add("active-chip");
+
+        productBox2.innerHTML = "";
+        
+        if(btn.value == "all"){
+            getProducts2();
+        }
+
+        else if(btn.value == "smartphones"){
+            smartProduct();
+        }
+
+        else if(btn.value == "laptops"){
+
+            laptopProduct();
+        }
+
+        else if(btn.value == "mobile-accessories"){
+
+            accessoriesProduct();
+        }
+    });
+});
